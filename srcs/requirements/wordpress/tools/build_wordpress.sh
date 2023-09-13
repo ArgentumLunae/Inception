7c9e919d73132fed5db53e989be118989bb792f1
Michiel Teerling <mteerlin@f1r4s18.codam.nl>
2023-09-13 18:04:08 +0200

    wordpress and mariadb containers start up, mariadb script has issues with stty: 'standard input': ioctl.

--- srcs/requirements/wordpress/tools/build_wordpress.sh
+++ srcs/requirements/wordpress/tools/build_wordpress.sh
@@ -1,21 +1,19 @@
-!/bin/sh
+#!/bin/bash
 
 if [-f ./wp-config.php]; then
 	echo "Wordpress is already downloaded"
 else
 	echo "downloading and extracting wordpress for $DOMAIN_NAME"
-	wget https://wordpres.org/latest.tar.gz
+	wget https://wordpress.org/latest.tar.gz
 	tar -xvzf latest.tar.gz
 	mv wordpress/* .
 	rm latest.tar.gz
 	rm -rf wordpress
 	echo "Download Coplete!"
 
-	# rm -rf wp-config.php
-	# cp /usr/loca/bin/wp-config.php
-
-	sed -i "s/username_here/$WB_DATABASE_USER/g" wp-config.php
-	sed -i "s/password_here/$WB_DATABASE_PASSWORD/g" wp-config.php
-	sed -i "s/database_name_here/$WB_DATABASE_NAME/g" wp-config.php
-	sed -i "s/local_here/$DOMAIN_NAME/g" wp-config.php
+	sed -i "s/username_here/$WB_DATABASE_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$WB_DATABASE_PASSWORD/g" wp-config-sample.php
+	sed -i "s/database_name_here/$WB_DATABASE_NAME/g" wp-config-sample.php
+	sed -i "s/local_here/$DOMAIN_NAME/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
 fi
