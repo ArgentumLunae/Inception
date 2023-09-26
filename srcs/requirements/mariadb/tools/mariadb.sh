b3a7f56d31b36c01ebaa2faa4aece611095594ab
Michiel Teerling <mteerlin@f1r4s20.codam.nl>
2023-09-26 19:32:19 +0200

    why?

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mariadb-install-db
+mysql_install_db
+mysql_upgrade
 /etc/init.d/mysql start
 
-if [ -f "var/lib/mysql/$MYSQL_DATABASE" ]
+if [ -d "var/lib/mysql/$MYSQL_DATABASE" ]
 then
 	echo "Database good and ready."
 else
@@ -14,8 +15,11 @@ else
 # nohup /usr/bin/mariadbd-safe --user=root --datadir=/var/lib/mysql > /dev/null &
 # bg_pid=$!
 
-echo "installing mysql"
-mysql_secure_installation <<END
+
+# echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+# echo "installing mysql"
+
+mysql_secure_installation --protocol=TCP <<END
 
 Y
 $MYSQL_ROOT_PASSWORD
@@ -26,10 +30,9 @@ Y
 Y
 END
 
-echo "mysql finished installing"
+# echo "mysql finished installing"
 
-# echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-echo "attempting to create database with user"
+# echo "attempting to create database with user"
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
 kill $bg_pid
