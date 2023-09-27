62999a55f56d5fa5e7d82967b7df6e0f21487752
Michiel Teerling <mteerlin@f1r4s20.codam.nl>
2023-09-27 19:29:53 +0200

    everything is terrible, but at least i'm past the MariaDB connection/ioctl issue

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
 mysql_install_db
-mysql_upgrade
-/etc/init.d/mysql start
+/etc/init.d/mariadb start
 
-if [ -d "var/lib/mysql/$MYSQL_DATABASE" ]
+
+if [ -f "var/lib/mysql/$MYSQL_DATABASE" ]
 then
 	echo "Database good and ready."
 else
 
-
-# chown -R mysql /var/lib/mysql
-
-# nohup /usr/bin/mariadbd-safe --user=root --datadir=/var/lib/mysql > /dev/null &
-# bg_pid=$!
-
-
-# echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-# echo "installing mysql"
-
-mysql_secure_installation --protocol=TCP <<END
+mysql_secure_installation <<END
 
 Y
 $MYSQL_ROOT_PASSWORD
@@ -30,16 +20,12 @@ Y
 Y
 END
 
-# echo "mysql finished installing"
-
 # echo "attempting to create database with user"
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
-kill $bg_pid
-
 fi
 
-/etc/init.d/mysql stop
+/etc/init.d/mariadb stop
 
 exec "$@"
 
